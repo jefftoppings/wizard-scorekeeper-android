b3200a2a784ad4efb746d7c8f9ca0a94847197f0
Jeffrey Toppings <jtt607@mail.usask.ca>
2019-01-17 12:20:58 -0600

    Created Main Activity for when app is opened

--- scorekeeper-app/app/src/main/java/jeff/wizardscorekeeper/MainActivity.java
+++ scorekeeper-app/app/src/main/java/jeff/wizardscorekeeper/MainActivity.java
@@ -1,7 +1,12 @@
 package jeff.wizardscorekeeper;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView title = (TextView)findViewById(R.id.title);
+        title.setBackgroundColor(Color.rgb(255,0,0));
+        title.setTextColor(Color.rgb(255,255,0));
+
+        LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainLayout);
+        mainLayout.setBackgroundColor(Color.rgb(0,0,255));
+
+        Button newGameButton = (Button) findViewById(R.id.newGameButton);
+        newGameButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
