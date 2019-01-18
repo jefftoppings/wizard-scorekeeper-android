fd2cdc1e57a50485c1735c08ca84d9dadb9282fe
Jeffrey Toppings <jtt607@mail.usask.ca>
2019-01-18 11:20:45 -0600

    Completed version 1 of newGame() function

--- scorekeeper-app/app/src/main/java/jeff/wizardscorekeeper/MainActivity.java
+++ scorekeeper-app/app/src/main/java/jeff/wizardscorekeeper/MainActivity.java
@@ -5,10 +5,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Space;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -46,7 +52,7 @@ public class MainActivity extends AppCompatActivity {
         // create new title
         Space topSpace = new Space(this);
         mainLayout.addView(topSpace);
-        topSpace.setMinimumHeight(100);
+        topSpace.setMinimumHeight(140);
         TextView newGameTitle = new TextView(this);
         mainLayout.addView(newGameTitle);
         newGameTitle.setMinHeight(200);
@@ -55,5 +61,51 @@ public class MainActivity extends AppCompatActivity {
         newGameTitle.setText(R.string.new_game);
         newGameTitle.setGravity(Gravity.CENTER);
         newGameTitle.setTextSize(20);
+
+        // ask how many players
+        Space midSpace = new Space(this);
+        mainLayout.addView(midSpace);
+        midSpace.setMinimumHeight(250);
+        TextView howMany = new TextView(this);
+        mainLayout.addView(howMany);
+        howMany.setText(R.string.how_many_players);
+        howMany.setTextSize(18);
+        howMany.setTextColor(Color.WHITE);
+
+        // put spinner to select how many players
+        List<String> array = new ArrayList<>();
+        for (int i=3; i<=6; i++) {
+            array.add(Integer.toString(i));
+        }
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, array);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        Spinner selectPlayers = new Spinner(this);
+        mainLayout.addView(selectPlayers);
+        selectPlayers.setAdapter(adapter);
+        selectPlayers.setGravity(Gravity.CENTER);
+        selectPlayers.setBackgroundColor(Color.WHITE);
+
+        // make okay button at bottom
+        Space botSpace = new Space(this);
+        mainLayout.addView(botSpace);
+        botSpace.setMinimumHeight(300);
+        Button okayButton = new Button(this);
+        mainLayout.addView(okayButton);
+        okayButton.setGravity(Gravity.CENTER);
+        okayButton.setText(R.string.ok);
+        okayButton.setWidth(2);
+        okayButton.setMinimumWidth(2);
+        okayButton.setMaxWidth(2);
+
+        okayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                get_names();
+            }
+        });
+    }
+
+    public void get_names() {
+        
     }
 }
